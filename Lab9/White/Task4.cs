26b0f7eb8d9e2d32fa80a6c83c481a23f2878bf3
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-05-18 00:18:31 +0300

    Update Task4.cs

--- Lab9/White/Task4.cs
+++ Lab9/White/Task4.cs
@@ -1,10 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsoleApp22
+namespace Lab9.White
 {
-    internal class Task4
+    public class Task4 : White
     {
+        private int _output;
+
+        public Task4(string text) : base(text)
+        {
+        }
+
+        public int Output
+        {
+            get
+            {
+                return _output;
+            }
+        }
+
+        public override void Review()
+        {
+            _output = CalculateSumOfDigits(Input);
+        }
+
+        private int CalculateSumOfDigits(string text)
+        {
+            int sum = 0;
+
+            foreach (char ch in text)
+            {
+                if (char.IsDigit(ch))
+                {
+                    sum += ch - '0';
+                }
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
     }
 }
